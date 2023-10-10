0e0028674da9c1d0cb80b062f465b34f0e2df3a9
shaloo <5890484+shaloo@users.noreply.github.com>
2023-10-10 11:15:23 +0530

    Refs: #AR-6972 Auth SDK v1.0.8 updated and verified

--- src/getArcanaAuth.js
+++ src/getArcanaAuth.js
@@ -5,7 +5,8 @@ let auth: AuthProvider | null;
 const getAuthProvider = () => {
   if (!auth) {
     auth = new AuthProvider(
-      "xar_test_b2dde12aad64eb35d72b2c80926338e178b1fa3f",
+      //"xar_test_b2dde12aad64eb35d72b2c80926338e178b1fa3f",
+      "xar_live_d7c88d9b033d100e4200d21a5c4897b896e60063",
       {
         theme: "light",
         connectOptions: {
